1c4bc8764594e71824a3e7509598da63d3da2515
Sandesh Devaraju <scouredimage@gmail.com>
2018-12-21 11:15:20 -0800

    day 8

---
+++ 8.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+)
+
+type node struct {
+	children []*node
+	metadata []int
+}
+
+func read() *node {
+	n := node{make([]*node, 0), make([]int, 0)}
+	var children, metadata int
+	fmt.Scan(&children)
+	fmt.Scan(&metadata)
+	for i := 0; i < children; i++ {
+		n.children = append(n.children, read())
+	}
+	for i := 0; i < metadata; i++ {
+		var m int
+		fmt.Scan(&m)
+		n.metadata = append(n.metadata, m)
+	}
+	return &n
+}
+
+func (n *node) sum() int {
+	val := 0
+	for _, m := range n.metadata {
+		val += m
+	}
+	for _, c := range n.children {
+		val += c.sum()
+	}
+	return val
+}
+
+func (n *node) value() int {
+	if len(n.children) == 0 {
+		val := 0
+		for _, m := range n.metadata {
+			val += m
+		}
+		return val
+	} else {
+		val := 0
+		for _, m := range n.metadata {
+			if m > 0 && m <= len(n.children) {
+				val += n.children[m-1].value()
+			}
+		}
+		return val
+	}
+}
+
+func main() {
+	root := read()
+
+	// What is the sum of all metadata entries?
+	fmt.Println(root.sum())
+
+	// What is the value of the root node?
+	fmt.Println(root.value())
+}
